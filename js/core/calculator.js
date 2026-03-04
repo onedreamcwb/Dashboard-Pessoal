32b1e0fb842c8f948c15781f604650fcd81311ff
onedreamcwb <gustavo.luanaraujo2018@gmail.com>
2026-03-04 06:36:35 -0300

    add calculator module for calculating transaction totals

---
+++ js/core/calculator.js
@@ -0,0 +1,6 @@
+const Calculator = {
+    calculateTotals: function (transactions) {
+        // Retorna totais zerados por enquanto para não dar erro
+        return { income: 0, expense: 0, total: 0 };
+    }
+};
